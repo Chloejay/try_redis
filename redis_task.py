f7a1513285e845b80b3b72bb76c94616213a8b90
chloejay <ji.jie@edhec.com>
2020-01-15 20:11:18 +0800

    add redis cache for kafka mysql database

--- redis_task.py
+++ redis_task.py
@@ -3,31 +3,46 @@ import logging
 import time 
 from pprint import pprint 
 import traceback 
+import pymysql 
 
 
-# TODO
-# CACHE_TTL= 60 *10 
-# implement the pipeline, transaction, cache
+CACHE_TTL= 60 *10
+HOST='localhost'
+USER='root'
+PSWD='password'
+DB='kafka_test'
+
 
 logging.basicConfig(level=logging.INFO) 
 conn= redis.Redis(host='localhost', port=6379) 
 start= time.time() 
 
-def get_test(r: redis.Redis): 
+sql= '''select favorite_color from kafkav1 where id=1'''
+
+def get_test(r: redis.Redis, sql, TTL, host, user, pswd, db): 
     with r.pipeline() as p: 
 
         try:
-            p.lpush("name", 'chloe')
-            p.lpush("name", 'emma')
-            p.lpop("name") 
+            # p.lpush("name", 'chloe')
+            # p.lpush("name", 'emma')
+            # p.lpop("name") 
+            mysql_conn=pymysql.connect(host, user, pswd, db) 
+            cursor=mysql_conn.cursor()
+            cursor.execute(sql)
+            data= cursor.fetchall() 
+            #TODO 
+            p.set('key', str(data))
+            p.expire('key', TTL) 
+            p.get('key') 
             result=p.execute() 
+
             logging.info(f'execute pipe {result}')
             return result 
 
-        except Exception as exc:
-            traceback.print_exc(exc) 
+        except Exception as exc: 
+            traceback.print_exc() 
             
 end = time.time() - start 
 pprint(f'execute time is {end}')  
 
-pprint(get_test(conn)) 
+pprint(get_test(conn, sql, CACHE_TTL, HOST,USER, PSWD, DB )) 
